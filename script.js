c81037edba311cbe70cdf3241092b3ef654d2dee
leye20 <leyeagbo@gmail.com>
2020-03-11 21:14:44 -0600

    my first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// Create an array of words
+const words = [ 'fat', 'lazy', 'hungary' ];
+// Select words randomly
+var selectedWords = Math.floor( Math.random() * words.length );
+console.log( 'hello world' );
+// Create the underscore based on length of word
+
+// Get users guess
+
+// Check if guess is right
+
+// if yes, push to right array
+
+// if no, push to wrong array
