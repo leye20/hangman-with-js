52d343ccc0f5560c64d3df1f2bf1ae1a48d90f59
leye20 <leyeagbo@gmail.com>
2020-03-12 01:11:32 -0600

    test commit

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
+// Set the number of chances
+var maxChance = 10;
+
 // Create an array of words
-const word = [ 'fat', 'lazy', 'hungary' ];
+var word = [ 'ultron', 'wanda', 'thanos', 'tchalla', 'hulk' ];
 // Select words randomly
 var randNum = Math.floor( Math.random() * word.length );
 var selectedWord = word[ randNum ];
+var rightWord = [];
+var wrongWord = [];
 var underScore = [];
+
+
+var docUnderScore = document.getElementsByClassName( 'underScore' );
+var docRightGuess = document.getElementsByClassName( 'rightGuess' );
+var docWrongGuess = document.getElementsByClassName( 'wrongGuess' );
+// Testing if it works
 console.log( selectedWord );
 // Create the underscore based on length of word
 var generateUnderscore = () => {
@@ -12,13 +23,30 @@ var generateUnderscore = () => {
   }
   return underScore;
 }
-console.log( generateUnderscore() );
+// console.log( generateUnderscore() );
+
 // Get users guess
 document.addEventListener( 'keypress', (event) => {
   var keyword = String.fromCharCode( event.keyCode );
-  console.log( keyword );
+  
+  if ( selectedWord.indexOf( keyword ) > -1 ) {
+    rightWord.push( keyword );
+    underScore[ selectedWord.indexOf( keyword ) ] = keyword;
+    docUnderScore[0].innerHTML = underScore.join( ' ' );
+    docRightGuess[0].innerHTML = rightWord;
+
+    if (underScore.join( '' ) == selectedWord) {
+      alert( 'You Win!' );
+    }
+  }
+  else {
+    wrongWord.push( keyword );
+    docWrongGuess[0].innerHTML = wrongWord;
+  }
 });
-// Check if guess is right
+
+docUnderScore[0].innerHTML = generateUnderscore().join( ' ' );
+// Check if guess is right. using the index method
 
 // if yes, push to right array
 
---
+++ test.js
@@ -0,0 +1,93 @@
+// Start Hangman Game code js
+var _this;
+var maxChances = 10;
+
+var gameController = {
+    form: document.querySelector('form'),
+    input: document.querySelector('[name="guess"]'),
+    word: document.querySelector('.word'),
+    chances: document.querySelector('.chances'),
+    pastGuesses: document.querySelector('.past-guesses')
+}
+
+function Hangman() {
+    this.runGame = true;
+    this.words = [
+        'ultron',
+        'thanos',
+        'wanda',
+        'tchalla',
+        'hulk'
+    ];
+    this.word = '';
+    this.displayString = '';
+    this.chances = maxChances;
+    this.pastGuesses = [];
+
+    this.run = function () {
+        this.setup();
+        _this = this;
+        gameController.form.addEventListener('submit', this.guessLetter);
+    };
+
+    this.setup = function () {
+        gameController.pastGuesses.innerHTML = '';
+        this.pastGuesses = [];
+        this.chances = maxChances;
+        this.displayString = '';
+        var i = Math.floor(Math.random() * this.words.length);
+        this.word = this.words[i];
+
+        for (var i = 0; i < this.word.length; i++) this.displayString += '_';
+        gameController.word.textContent = this.displayString;
+        gameController.chances.textContent = this.chances;
+        console.log(this.word);
+    };
+
+    this.guessLetter = function (event) {
+        event.preventDefault();
+        if (_this.word.includes(gameController.input.value)) {
+            for (var i = 0; i < _this.word.length; i++) {
+               var currentChar = _this.word.substr(i, 1);
+                if (currentChar === gameController.input.value) {
+
+                    _this.displayString =
+                        _this.displayString.slice(0, i) +
+                        currentChar +
+                        _this.displayString.slice(i + 1, _this.displayString.length);
+
+                    gameController.word.textContent = _this.displayString;
+                }
+            }
+
+            //Solved puzzle?
+            if (!gameController.word.textContent.includes('_')) {
+                _this.win();
+            }
+        } else {
+            _this.chances--;
+            gameController.chances.textContent = _this.chances;
+
+            if (_this.chances == 0) {
+                _this.lose();
+            }
+        }
+      gameController.input.value = '';
+    };
+
+    this.win = function () {
+        if (confirm('Excellent! Play again?')) {
+            this.setup();
+        }
+    };
+
+    this.lose = function () {
+       if (confirm('Watch More Movies and Try again?')) {
+          this.setup();
+        }
+    };
+};
+
+
+var game = new Hangman();
+game.run();
